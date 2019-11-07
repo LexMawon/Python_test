538eee75f00a6d60fb71cfd20c52945ee7c2592b
Aleksey <44863762+LexMawon@users.noreply.github.com>
2019-11-07 13:41:19 +0400

    Форматирование текста

---
+++ format_str2.py
@@ -0,0 +1,17 @@
+empty_list = []
+# пустой список
+
+friend = ['Max', 'Leo', 'Kot']
+# список 3х друзей
+
+prints = (type(friend))
+# узнаем тип
+print(prints)
+
+print('Первое имя из списка: ', friend[0])
+print('Последнее имя из списка: ', friend[2])
+
+#срезы
+
+print(friend[:2])
+print(friend[1:])
